d1d157c587b54c1f14a9d819b4cfc459112976c8
Javier Castro <jaca2899@gmail.com>
2020-02-16 15:54:54 -0500

    Allowed all hosts in `settings.py`.

--- danetiska/settings.py
+++ danetiska/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@bhk+2)uz(@)v_=lf)o9catn+&!fmqeky+9syiucgwy(8ls!gp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
