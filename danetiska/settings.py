5a235ad2c5773ea8ea5bdaa4b7134c40055524b9
Javier Castro <jaca2899@gmail.com>
2020-02-16 14:03:05 -0500

    Told `settings.py` where the templates are stored.

--- danetiska/settings.py
+++ danetiska/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'danetiska.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'danetiska/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
